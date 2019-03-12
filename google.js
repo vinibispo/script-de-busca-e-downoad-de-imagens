c7f1e484ec89a70d48644880818a7ca967ca2092
vinibispo <vini.bispo015@gmail.com>
2019-03-12 08:50:46 -0300

    google api instalacao

--- google.js
+++ google.js
@@ -1,1 +1,7 @@
-const google = require('googleapi');
+function search(){
+	console.log("sou a função search")
+	const google = require('google')
+	
+}
+module.exports = search
+
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const readline  = require('readline-sync');
+const readline  = require('readline-sync')
+const google = require('./google.js')
+
 function start() {
-	const contentsearch = {};
-	contentsearch.searchTerm = askAndReturnSearchTerm();
+	const contentsearch = {}
+	contentsearch.searchTerm = askAndReturnSearchTerm()
 
 	function askAndReturnSearchTerm() {
-	 	return readline.question('Type a search term: ');
-	 	} ;
-	 console.log(contentsearch);
+	 	return readline.question('Type a search term: ')
+	}
+	console.log(contentsearch)
+	 
+	google()
 };
 
-start();
+start()
