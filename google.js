c4f279c3ddab3c5b9ad4447cabac999896a7d465
vinibispo <vini.bispo015@gmail.com>
2019-03-12 09:36:15 -0300

    codigo google falso

--- google.js
+++ google.js
@@ -1,7 +1,23 @@
 function search(search){
-	const google = require('googleapi')
-	
-
+	let google = require('google')
+	 
+	google.resultsPerPage = 1
+	let nextCounter = 0
+	 
+	google(search, function (err, res){
+	  if (err) console.error(err)
+	 
+	  for (let i = 0; i < res.links.length; ++i) {
+		let link = res.links[i]
+		console.log(link.title + ' - ' + link.href)
+		
+	  }
+	 
+	  if (nextCounter < 4) {
+		nextCounter += 1
+		if (res.next) res.next()
+	  }
+	})
 }
 module.exports = search
 
