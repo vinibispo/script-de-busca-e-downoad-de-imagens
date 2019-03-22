3ceda829c9c9bb03f68d0c311b811a1b55595ede
gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-22 14:05:18 -0300

    codigo de criar arquivo docx

--- word.js
+++ word.js
@@ -1,3 +1,20 @@
 const docx = require('docx')
-var fs = require("fs")
-var docx = require("docx")
+const fs = require("fs")
+
+
+async function start() {
+	const doc = new docx.Document()
+
+	const paragraph = new docx.Paragraph("Hello World")
+	
+
+	doc.addParagraph(paragraph)
+
+	const packer = new docx.Packer()
+
+	packer.toBuffer(doc).then((buffer) => {
+		fs.writeFileSync('test.docx', buffer)
+	})
+}
+
+start()
