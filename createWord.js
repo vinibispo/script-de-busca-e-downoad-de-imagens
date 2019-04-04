d2a917adb3e47a2e28836943db008c6831db996f
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-04 14:21:14 -0300

    centrally defined styles cover

--- createWord.js
+++ createWord.js
@@ -3,27 +3,26 @@
 class word {
   constructor() {
     this.docx = require('docx')
-    this.styles = new this.docx.Styles()
     this.doc = new this.docx.Document()
     this.fs = require('fs')
 
-    this.styles.createParagraphStyle('normal')
-      .spacing({line: 1000})
+    this.doc.Styles
+      .createParagraphStyle('cover')
+      .size(50) 
   }
 
   start(parametersObj) {
     this.par = parametersObj
     this.fileTitle = this.par.title
 
-    this.doc.addParagraph(new this.docx.Paragraph().center().addRun(new this.docx.TextRun('ETEC Vasco Antônio Venchiarutti').size(24).font('Arial')))
-
+    this.doc.addParagraph(new this.docx.Paragraph('ETEC Vasco Antônio Venchiarutti').style('cover'))
 
     this.create()
   }
 
   create() {
-    new this.docx.Packer().toBuffer(this.doc).then((buffer) => {
-      this.fs.writeFileSync(this.fileTitle, buffer)
+      new this.docx.Packer().toBuffer(this.doc).then((buffer) => {
+        this.fs.writeFileSync(this.fileTitle, buffer)
     })
   }
 }
