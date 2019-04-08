5a5eacbba9d1b72168d0a059ddc2047661c9c981
vinibispo <vini.bispo015@gmail.com>
2019-04-07 22:21:18 -0300

    catching very results

--- algorithmia.js
+++ algorithmia.js
@@ -3,11 +3,10 @@ const pass = require('./credentials/package.json').algo
 //const google = require('./google')
 async function Summarize(link){
     console.log(link)
-    algorithmia.client(pass)
-    .algo("nlp/SummarizeURL/0.1.4?timeout=300") // timeout is optional
-    .pipe(link)
-    .then(function(response) {
-        console.log(response.get())
-    });
+    const algoAuthenticated = await algorithmia(pass)
+   const SummarizeURL = await algoAuthenticated.algo("nlp/SummarizeURL/0.1.4?timeout=300") // timeout is optional
+    const SummarizeResponse = await SummarizeURL.pipe(link)
+    const SummarizeContent = await SummarizeResponse.get()
+    return SummarizeContent
 }
 module.exports = Summarize
--- google.js
+++ google.js
@@ -5,15 +5,19 @@ async function Google(searchterm, count){
 	const pass = require('./credentials/package.json').googlesearch
 	const id = require('./credentials/package.json').customid
 	const answer = await Customsearch.cse.list({auth: pass, cx:id, q: searchterm, num: count})
+	SummarizedList = []
 	for(let i = 0; i < count; i++){
 		let link = answer.data.items[i].link
 		if (link.indexOf('wikipedia') > -1 || link.indexOf('youtube') > -1){
 		} else if(link.indexOf(' ') >-1){
 			link = link.replace(' ', '')
-			summarizedContent = await Summarize(link)
+			const summarizedContent = await Summarize(link)
+			SummarizedList.push(summarizedContent)
 		}else{
-			summarizedContent = await Summarize(link)
+			const summarizedContent = await Summarize(link)
+			SummarizedList.push(summarizedContent)
 		}
-}
+	}
+	return SummarizedList
 }
 module.exports = Google
