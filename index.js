d404318865c8a2f58393233c3bfb19b9d5ba48d4
vinibispo <vini.bispo015@gmail.com>
2019-04-16 21:21:31 -0300

    getting authors and the name of the docx

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ async function start() {
 	const contentsearch = {}
 	contentsearch.searchTerm = askAndReturnSearchTerm()
 	contentsearch.amount = askAndReturnAmount()
+	contentsearch.results = await findResults()
 
 	function askAndReturnSearchTerm() {
-	 	return readline.question('Type a search term: ')
+		 question = readline.question('Type a search term: ')
+		 return question
 	}
-	console.log(contentsearch)
 	function askAndReturnAmount(){
 		return readline.question('Type how many results do you wanna have: ')
 	}
-	 
-	content = await google(contentsearch.searchTerm, contentsearch.amount)
-	 return content
+	async function findResults(){
+		content = await google(contentsearch.searchTerm, contentsearch.amount)
+		return content
+	}
+	return contentsearch
+	
 	/*
 	let obj = {
 		fileName: 'test.docx', 
@@ -41,5 +45,4 @@ async function start() {
 
 	word.start(obj)*/
 }
-
-module.exports = start
+module.exports = start()
--- input.js
+++ input.js
@@ -1,16 +1,42 @@
 const date = new Date()
 const word = require('./createWord')
 const read = require('readline-sync')
-name = read.question('Type the name of the file: ')
-const obj = {
-    authors: [
-        'Gustavo',
-        'Vinícius',
-      ],
+async function Index(){
+  content = await require('./index')
+  return content
+}
+async function nameofFile(){
+  content = await Index()
+  name = content.searchTerm
+  return name
+}
+async function getAllContent(){
+  content = await Index()
+  cont = content.results
+  return cont
+}
+async function getAuthors(){
+  amount = read.question('Type how many authors that has: ')
+  authors = []
+  for(let i = 0; i< amount; i++){
+    author = read.question('Type the name of author: ')
+    authors.push(author)
+  }
+  return authors
+}/*
+async function createWord(){
+  const obj = {
+    authors: await getAuthors(),
+      fileName : await nameofFile() + '.docx',
       title: 'Título',
       subTitle: 'SubTítulo',
       place: 'Jundiaí',
-      year: date.getFullYear(),
+      year: date.getFullYear()
 }
-obj.fileName = name + '.docx'
 word.start(obj)
+}
+createWord()*/
+async function start(){
+  console.log(await getAllContent())
+}
+start()
