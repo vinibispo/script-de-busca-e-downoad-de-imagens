fe995a20e89cb763482f3e92a81d0ab7c5beaecf
vinibispo <vini.bispo015@gmail.com>
2019-04-25 05:54:40 -0300

    putting googleapis again

--- image.js
+++ image.js
@@ -1,4 +1,15 @@
-async function getImages(){
-    
+async function getListImages(searchterm){
+    const google= require('googleapis').google
+	const Customsearch = google.customsearch('v1')
+	const pass = require('./credentials/package.json').googlesearch
+	const id = require('./credentials/package.json').imgsearch
+    const answer = await Customsearch.cse.list({auth: pass, cx:id, q: searchterm, num: 2})
+    console.dir(answer)
 }
+async function getImages(searchterm){
+    const getImages = await getListImages(searchterm)
+    console.log(getImages)
+}
+getListImages('bolo de cenoura')
+
 module.exports = getImages
--- index.js
+++ index.js
@@ -25,6 +25,5 @@ async function start() {
 		return keywords
 	}
 	return contentsearch
-
 }
 module.exports = start()
