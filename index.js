e0bec334e491ab8167fa9c73532b9e3a22eb4f84
Vinícius Bispo <48097622+vinibispo@users.noreply.github.com>
2019-04-29 23:49:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function start() {
 		maximumSentences: 10
 	}
 	robots.userInput(content)
+        console.log('Welcome, you are on index')
 	await robots.text(content)
 	await robots.google(content)
 	await robots.image(content)
