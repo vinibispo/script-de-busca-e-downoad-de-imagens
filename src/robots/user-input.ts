fdeb81d0e02891e22d7c517c2eef687ef45b9eee
vinibispo <vini.bispo015@gmail.com>
2020-03-08 14:31:58 -0300

    I've had a mistake in english in user-input

--- src/robots/user-input.ts
+++ src/robots/user-input.ts
@@ -3,8 +3,8 @@ const robot = (content:any) =>{
 console.log("[input-robot] Starting input robot")
  const askAndReturnSearchTerm = ( ) =>{
     console.log("[input-robot] Welcome to the input robot")
-    const therm =  read.question("What do you look for?")
-    console.log(`[input-robot] Get therm ${therm}`)
+    const term =  read.question("What do you look for?")
+    console.log(`[input-robot] Got term ${term}`)
 }
 const askAndReturnNumOfResults = () =>{
     const amount = read.questionInt('[input-robot] How much results do you have?')
