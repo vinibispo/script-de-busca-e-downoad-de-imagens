91e922508940097dc95c5dfa2199c1533803ea63
vinibispo <vini.bispo015@gmail.com>
2020-03-08 14:31:50 -0300

    I've had a mistake in english in user-input

--- src/robots/google.ts
+++ src/robots/google.ts
@@ -1,2 +1,9 @@
 import {google } from 'googleapis'
 const Google = google.customsearch('v1')
+
+const robot = async(content: any) =>{
+    const fetchLinksFromGoogle = (content) =>{
+        const answer = await Google.cse.list({auth: process.env.GOOGLE_PASS, cx: process.env.GOOGLE_ID})
+    }
+    await fetchLinksFromGoogle(content)
+}
--- src/robots/user-input.ts
+++ src/robots/user-input.ts
@@ -1,7 +1,7 @@
 import read from 'readline-sync'
 const robot = (content:any) =>{
 console.log("[input-robot] Starting input robot")
- const askAndReturnSearchTherm = ( ) =>{
+ const askAndReturnSearchTerm = ( ) =>{
     console.log("[input-robot] Welcome to the input robot")
     const therm =  read.question("What do you look for?")
     console.log(`[input-robot] Get therm ${therm}`)
@@ -10,7 +10,7 @@ const askAndReturnNumOfResults = () =>{
     const amount = read.questionInt('[input-robot] How much results do you have?')
     return amount
 }
-    content.therm = askAndReturnSearchTherm()
+    content.term = askAndReturnSearchTerm()
     content.amount = askAndReturnNumOfResults()
 }
 export default robot
