200b3529ffdc041a78c917dead5edd2d0837de95
vinibispo <vini.bispo015@gmail.com>
2020-03-08 18:12:14 -0300

    we could download images

--- src/robots/image.js
+++ src/robots/image.js
@@ -4,16 +4,41 @@ dotenv.config({path: '.env'})
 const {google } = require( 'googleapis')
 const Google = google.customsearch('v1')
 const robot = async() =>{
+    console.log('[image-robot] starting image robot')
     const content = state.load()
     const fetchLinksFromGoogle = async(content) =>{
-        url = []
-        const answer = await Google.cse.list({key: process.env.GOOGLE_PASS, q:content.term, cx: process.env.GOOGLE_ID, num: content.amount, searchType: 'image', imgSize: 'huge'})
-        for(let i = 0; i < content.amount; i++){
-            url.push(answer.data.items[i].link)
-        } 
-        return url
+        console.log('[image-robot] getting links from image')
+        const url = []
+        const answer = await Google.cse.list({key: process.env.GOOGLE_PASS, q:content.term, cx: process.env.GOOGLE_ID, num: content.amount, searchType: 'image'})
+        answer.data.items.map(item => { 
+            console.log(`[image-robot] getting link ${item.link}`)
+            url.push(item.link)})
+            return url
+    }
+    const downloadImageFromLink = async(content) =>{
+        const downloadAndSave = async(url, content, index) =>{
+            const download = require('image-downloader')
+            return await download.image({
+                dest: `./img/original/${content.term}${index}.jpg`,
+                url: `${url}`
+            })
+        }
+        const urlDownload = []
+        content.url.map(async(link, index) =>{
+            console.log(`[image-robot] downloading image ${link}`)
+            if(urlDownload.includes(link)){
+                console.log(`[image-robot] image already downloaded`)
+            }
+            else{
+            await downloadAndSave(link, content, index)
+            console.log(`[image-robot] downloaded image ${link}`)
+            urlDownload.push(link)
+            }
+        })
+        return urlDownload
     }
     content.url =  await fetchLinksFromGoogle(content)
+    content.urlDownload = await downloadImageFromLink(content)
     state.save(content)
 }
 module.exports = robot
