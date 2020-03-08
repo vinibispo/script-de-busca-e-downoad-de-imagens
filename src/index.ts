778b7ef668e36be924f838e56bfdcbad57e9815d
vinibispo <vini.bispo015@gmail.com>
2020-03-08 09:24:52 -0300

    adding tsc for my project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import userinput from './robots/user-input'
+const robots = {
+userinput,
+// google: require('./robots/google'),
+// text: require('./robots/sentences'),
+// image: require('./robots/image'),
+// wordinput: require('./robots/word-input'),
+// word: require('./robots/word')
+}
+
+async function start() {
+	// robots.userinput()
+	// await robots.text()
+	// await robots.google()
+	// await robots.image()
+	// robots.wordinput()
+	// robots.word()
+}
+
+start()
---
+++ src/robots/user-input.ts
@@ -0,0 +1,4 @@
+import read from 'readline-sync'
+const input = read.question('Qual é o seu nome? ')
+console.log(input)
+export default input
